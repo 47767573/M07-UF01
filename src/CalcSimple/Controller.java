c599f3b92e9170e01c8134b15633773e823a428c
Moises <47767573t@iespoblenou.org>
2015-10-09 18:40:31 +0200

    Practica de calculadora simple

--- src/CalcSimple/Controller.java
+++ src/CalcSimple/Controller.java
@@ -9,22 +9,68 @@ import javafx.scene.control.TextField;
 public class Controller {
 
     @FXML
-    private Button btnUno,btnDos,btnTres,btnCuatro,btnCinco,btnSeis,btnSiete,
-                btnOcho,btnNueve,btnCero,btnClear,btnDot,btnResultado,btnSuma,
-                btnResta,btnDividir,btnMultiplicar;
+    private TextField tfPantalla, tfOperacion;
 
-    private TextField tfPantalla;
-
-
-    String num1;
-    String num2;
+    String num1 = "";
+    String num2 = "";
+    String symbol;
 
 
     public void OnCalcular(ActionEvent actionEvent) {
         String clickNumero = ((Button)actionEvent.getSource()).getText();
 
-        num1 += (clickNumero);
-        tfPantalla.setText(num1);
+        if (symbol == null) {
+            num1 += clickNumero;
+            tfPantalla.setText(num1);
+        } else if (symbol != null) {
+            num2 += clickNumero;
+            tfPantalla.setText(num2);
+        }
     }
 
+    public void OnOperacion(ActionEvent actionEvent) {
+        String clickOperacion = ((Button)actionEvent.getSource()).getText();
+
+        symbol = clickOperacion;
+        tfOperacion.setText(symbol);
+
+        if (num2 != null){
+            OnIgual(actionEvent);
+        }
+    }
+
+    public void OnIgual(ActionEvent actionEvent) {
+        Double dbNum1;
+        Double dbNum2;
+        Double total = 0.0;
+
+        dbNum1 = Double.valueOf(num1);
+        dbNum2 = Double.valueOf(num2);
+
+        if (symbol.equals("x")) {
+            total = dbNum1*dbNum2;
+        }else if (symbol.equals("/")) {
+            total = dbNum1/dbNum2;
+        }else if (symbol.equals("+")) {
+            total = dbNum1+dbNum2;
+        }else if (symbol.equals("-")) {
+            total = dbNum1-dbNum2;
+        }
+
+        num1 = String.valueOf(total);
+        tfPantalla.setText(String.valueOf(total));
+        num2="";
+
+    }
+
+    public void OnDeleteUnit(ActionEvent actionEvent) {
+        String numNuevo;
+
+        String numAntes =  tfPantalla.getText();
+        if (numAntes.length() >= 0) {
+            numNuevo = numAntes.substring(0, (numAntes.length() - 2));
+        }else{
+            tfPantalla.setText ("0");
+        }
+    }
 }
