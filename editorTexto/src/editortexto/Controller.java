2030a3e748fe671a0dabf389655c397eb4f13373
Moises <47767573t@iespoblenou.org>
2015-10-23 18:39:32 +0200

    proyecto editor Texto mejorado

--- editorTexto/src/editortexto/Controller.java
+++ editorTexto/src/editortexto/Controller.java
@@ -2,38 +2,77 @@ package editortexto;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.text.Font;
+import javafx.scene.image.ImageView;
 
 public class Controller  {
 
-    public MenuItem salir;
+    //TEXTAREA
     public TextArea taEditor;
-    public String selectedText="paco";
 
+    //BUTTONS - TOOLBAR
+    public Button btCopy;
+
+    //MENU ITEMS
+    public MenuItem miSalir;
+
+    public MenuItem miCopiar;
+    public MenuItem miCortar;
+    public MenuItem miPegar;
+    public MenuItem miDeshacer;
+
+    public MenuItem miArial;
+    public MenuItem miFreemono;
+    public MenuItem miFontSize20;
+    public MenuItem miFontSize14;
+    public MenuItem miFontSize11;
+
+    public MenuItem miAyuda;
+
+
+    //INICIO ---------------------------------------------------------------------------------
+    public void initialize(){
+
+    }
+
+
+
+    //Funciones FICHERO
     public void OnSalir (ActionEvent actionEvent){
         Platform.exit();
     }
 
+
+    //Funciones EDITAR
     public void OnCopiar (ActionEvent actionEvent){
         taEditor.copy();
     }
 
-    public void OnCortar(ActionEvent actionEvent) {
-        selectedText = taEditor.getSelectedText();
-        taEditor.cut();
-    }
+    public void OnCortar(ActionEvent actionEvent) { taEditor.cut();  }
 
     public void OnPegar(ActionEvent actionEvent) {
         taEditor.paste();
     }
 
     public void OnDeshacer(ActionEvent actionEvent){
-
+        taEditor.undo();
     }
 
-    public void OnChangeFont (ActionEvent actionEvent){
-        taEditor.getSelectedText();
+
+    //Funciones OPCIONES
+    public void setFontArial (ActionEvent actionEvent){
+        taEditor.setFont(new Font("SansSerif", taEditor.getFont().getSize()));
+    }
+    public void setFontTimes(ActionEvent actionEvent) {
+        taEditor.setFont(new Font("FreeMono", taEditor.getFont().getSize()));
+    }
+
+    public void setFontSize(ActionEvent actionEvent) {
+        Double fontSize = Double.parseDouble(((MenuItem) actionEvent.getSource()).getText());
+        taEditor.setFont(new Font(taEditor.getFont().getSize()));
     }
 
 
